7ce35f7efc69fe7a698fcdfae1f2b33e93192971
Ricky for RC <rongcloud@Ricky-for-RongCloudMacBook-Pro.local>
2025-07-28 21:31:21 +0800

    修复页面刷新空白问题：使用HashRouter替代BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import 'dayjs/locale/zh-cn'
@@ -9,10 +9,10 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/H5Demo-AI-Character-Admin">
+    <HashRouter>
       <ConfigProvider locale={zhCN}>
         <App />
       </ConfigProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 ) 
